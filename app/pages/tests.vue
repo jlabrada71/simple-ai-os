9bd66a0a90b68e8ec32da11c5c9beee87941579a
Juan Labrada <agile@juanlabrada.com>
2026-07-15 17:05:11 -0300

    feature: create a specific test page

---
+++ app/pages/tests.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Welcome to My App</h1>
+    <p>This is a simple Vue.js app.</p>
+    <div class="flex gap-4">    
+        <NuxtLink to="/chat-streaming" class="p-2 rounded bg-blue-300">Chat Streaming</NuxtLink>
+        <NuxtLink to="/stream-test" class="p-2 rounded bg-green-300">Stream Test</NuxtLink>
+        <NuxtLink to="/chat" class="p-2 rounded bg-purple-300">Chat</NuxtLink>
+        <NuxtLink to="/prompts" class="p-2 rounded bg-yellow-300">Prompts</NuxtLink>
+    </div>
+  </div>
+
+</template>
